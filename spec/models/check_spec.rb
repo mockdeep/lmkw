d949882135750485348b8fb486babd23df6f74ff
Robert Fletcher <lobatifricha@gmail.com>
2020-04-12 15:00:22 -0700

    Jobs: introduce CheckCount class
    This will be used to store the value of checks rather than looking them
    up on the fly. Next up we need to start populating it.

--- spec/models/check_spec.rb
+++ spec/models/check_spec.rb
@@ -3,6 +3,11 @@
 require "rails_helper"
 
 RSpec.describe Check, type: :model do
+  it { is_expected.to belong_to(:integration) }
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_many(:counts).class_name('CheckCount') }
+  it { is_expected.to validate_presence_of(:user_id) }
+
   describe "#refresh" do
     it "raises a NotImplementedError" do
       expect { described_class.new.refresh }
