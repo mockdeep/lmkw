0aa8736af90c8e8f6b368a694cd47f50cd3f4b29
Robert Fletcher <mockdeep@users.noreply.github.com>
2020-10-22 20:41:36 -0700

    Redirect to `/checks` when logged in (#444)
    Skip the Welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,11 @@
 
 class WelcomeController < ApplicationController
   skip_before_action(:authenticate_user)
+  before_action(:redirect_if_logged_in)
+
+  private
+
+  def redirect_if_logged_in
+    redirect_to(checks_path) if current_user.logged_in?
+  end
 end
