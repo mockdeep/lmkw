a29b5e167379e10a6f31fc1f4cf07ac3b9bfe1a8
Robert Fletcher <mockdeep@users.noreply.github.com>
2021-01-17 20:50:02 -0800

    refresh every 5 minutes (#471)
    This adds a `meta` tag to automatically refresh the page every 5
    minutes, as well as reducing the refresh window for checks from 1 hour
    to 5 minutes. The `meta` tag is maybe a tad hacky, but it's a good quick
    alternative before we get more ajaxy stuff in place.

--- spec/actions/check/run_all_outdated_spec.rb
+++ spec/actions/check/run_all_outdated_spec.rb
@@ -3,15 +3,15 @@
 require "rails_helper"
 
 RSpec.describe Check::RunAllOutdated do
-  it "runs checks that have not been updated in an hour" do
-    check = create_check(counts: [{ created_at: 61.minutes.ago }])
+  it "runs checks that have not been updated in the last five minutes" do
+    check = create_check(counts: [{ created_at: 6.minutes.ago }])
 
     expect { described_class.call }
       .to change(check.counts, :count).by(1)
   end
 
-  it "does not run checks that have been updated in the last hour" do
-    check = create_check(counts: [{ created_at: 59.minutes.ago }])
+  it "does not run checks that have been updated in the last five minutes" do
+    check = create_check(counts: [{ created_at: 4.minutes.ago }])
 
     expect { described_class.call }
       .not_to change(check.counts, :count)
