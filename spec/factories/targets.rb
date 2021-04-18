141cce95997e99bbe92b39fa075709f41fe92933
Robert Fletcher <lobatifricha@gmail.com>
2021-04-18 14:53:00 -0700

    Factories: switch count_value -> value
    That way I can cram more onto one line.

--- spec/factories/targets.rb
+++ spec/factories/targets.rb
@@ -3,10 +3,10 @@
 FactoryBot.define do
   factory(:target, class: "Check::Target") do
     transient do
-      count_value { nil }
+      check_value { nil }
     end
 
-    check { association(:check, target: instance, count_value: count_value) }
+    check { association(:check, target: instance, value: check_value) }
 
     trait(:refreshable) do
       next_refresh_at { 1.day.ago }
