f800a3c2a96d6444b6dd988510b9039360b0cd4d
Robert Fletcher <mockdeep@users.noreply.github.com>
2021-12-21 10:15:42 -0800

    update auto-generated files for Rails to 6.1 versions (#626)

--- config.ru
+++ config.ru
@@ -7,3 +7,4 @@ $stdout.sync = true
 require_relative "config/environment"
 
 run Rails.application
+Rails.application.load_server
--- config/application.rb
+++ config/application.rb
@@ -11,16 +11,26 @@ Bundler.require(*Rails.groups)
 module LetMeKnowWhen
 end
 
+# Configuration for the application, engines, and railties goes here.
+#
+# These settings can be overridden in specific environments using the files
+# in config/environments, which are processed later.
+#
+# config.time_zone = "Central Time (US & Canada)"
+# config.eager_load_paths << Rails.root.join("extras")
 class LetMeKnowWhen::Application < Rails::Application
   # Initialize configuration defaults for originally generated Rails version.
   config.load_defaults(6.0)
 
-  config.active_job.queue_adapter = :sidekiq
+  # Configuration for the application, engines, and railties goes here.
+  #
+  # These settings can be overridden in specific environments using the files
+  # in config/environments, which are processed later.
+  #
+  # config.time_zone = "Central Time (US & Canada)"
+  # config.eager_load_paths << Rails.root.join("extras")
 
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration can go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded after loading
-  # the framework and any gems in your application.
+  config.active_job.queue_adapter = :sidekiq
 
   config.active_record.belongs_to_required_by_default = false
   config.action_view.form_with_generates_remote_forms = false
