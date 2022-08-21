257442b2d44b41a2e7ea18b47470bcb7a29d2517
Robert Fletcher <mockdeep@users.noreply.github.com>
2022-08-20 20:28:53 -0700

    Trello: remove remaining ruby trello references (#767)

--- app/api_wrappers/n_trello/board.rb
+++ app/api_wrappers/n_trello/board.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class NTrello::Board
-  attr_accessor :id, :url
+  attr_accessor :id, :name, :url
 
-  def initialize(id:, url:)
+  def initialize(id:, name:, url:)
     self.id = id
+    self.name = name
     self.url = url
   end
 end
