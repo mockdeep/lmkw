f48d7e64aacd7359a1630591e522c01e758a700b
Robert Fletcher <lobatifricha@gmail.com>
2022-08-22 16:08:31 -0700

    Trello: refactor client specs

--- spec/api_wrappers/n_trello/client_spec.rb
+++ spec/api_wrappers/n_trello/client_spec.rb
@@ -3,10 +3,25 @@
 require "rails_helper"
 
 RSpec.describe NTrello::Client do
+  delegate :developer_public_key, to: :described_class
+
+  def auth_params
+    { key: developer_public_key, token: "blah" }
+  end
+
+  def boards_url
+    params = { filter: "open", **auth_params }
+
+    "https://api.trello.com/1/members/bo/boards?#{params.to_query}"
+  end
+
+  def member_url
+    "https://api.trello.com/1/members/me?#{auth_params.to_query}"
+  end
+
   describe ".authorize_url" do
     it "returns a URL to authorize Trello access" do
       return_url = "https://foobar.com/baz"
-      developer_public_key = described_class.developer_public_key
 
       result = described_class.authorize_url(return_url:)
 
@@ -16,21 +31,10 @@ RSpec.describe NTrello::Client do
   end
 
   describe "#fetch_boards" do
-    def response(uri:, body:)
-      body = String.new(body)
-      HTTP::Response.new(status: 200, version: "1.1", uri:, body:)
-    end
-
     it "returns boards" do
       client = described_class.new(member_token: "blah")
-      developer_public_key = described_class.developer_public_key
-      auth_params = { key: developer_public_key, token: "blah" }
-      params = { filter: "open", **auth_params }
 
-      boards_url = "https://api.trello.com/1/members/boo/boards?#{params.to_query}"
-      member_url = "https://api.trello.com/1/members/me?#{auth_params.to_query}"
-      stub_request(:get, member_url)
-        .to_return(body: { username: "boo" }.to_json)
+      stub_request(:get, member_url).to_return(body: { username: "bo" }.to_json)
       stub_request(:get, boards_url).to_return(body: [].to_json)
 
       expect(client.fetch_boards).to eq([])
@@ -40,7 +44,7 @@ RSpec.describe NTrello::Client do
   describe "#fetch_board" do
     it "returns the board with the given id" do
       client = described_class.new(member_token: "blah")
-      stub_request(:get, "https://api.trello.com/1/boards/3?key=b151cfc72ed56c15f13296ffbaf96194&token=blah")
+      stub_request(:get, "https://api.trello.com/1/boards/3?#{auth_params.to_query}")
         .to_return(body: { id: 3, url: "/bloo", name: "bloo board" }.to_json)
 
       result = client.fetch_board(id: 3)
@@ -52,7 +56,7 @@ RSpec.describe NTrello::Client do
   describe "#fetch_lists" do
     it "returns lists with the given board id" do
       client = described_class.new(member_token: "blah")
-      stub_request(:get, "https://api.trello.com/1/boards/3/lists?key=b151cfc72ed56c15f13296ffbaf96194&token=blah")
+      stub_request(:get, "https://api.trello.com/1/boards/3/lists?#{auth_params.to_query}")
         .to_return(body: [{ id: 4, name: "list 1" }].to_json)
 
       result = client.fetch_lists(board_id: 3)
@@ -64,7 +68,7 @@ RSpec.describe NTrello::Client do
   describe "#fetch_cards" do
     it "returns cards with the given list id" do
       client = described_class.new(member_token: "blah")
-      stub_request(:get, "https://api.trello.com/1/lists/5/cards?key=b151cfc72ed56c15f13296ffbaf96194&token=blah")
+      stub_request(:get, "https://api.trello.com/1/lists/5/cards?#{auth_params.to_query}")
         .to_return(body: [{ id: 6, name: "card 6" }].to_json)
 
       result = client.fetch_cards(list_id: 5)
