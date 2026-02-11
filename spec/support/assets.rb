1f1000d63e286b1e29f43834282cc0b8b4296717
Robert Fletcher <lobatifricha@gmail.com>
2026-02-10 20:19:09 -0800

    switch to pnpm for package management

--- spec/support/assets.rb
+++ spec/support/assets.rb
@@ -2,6 +2,6 @@
 
 RSpec.configure do |config|
   config.before(:all) do
-    system("yarn build > /dev/null 2>&1", exception: true)
+    system("pnpm build > /dev/null 2>&1", exception: true)
   end
 end
